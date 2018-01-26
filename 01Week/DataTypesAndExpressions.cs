37e2764b2cf225532e930994b7b32302dfe2e38c
j-earhart <31858509+j-earhart@users.noreply.github.com>
2018-01-25 18:25:21 -0800

    add new code

--- 01Week/DataTypesAndExpressions.cs
+++ 01Week/DataTypesAndExpressions.cs
@@ -1,10 +1,37 @@
-using System;
-using System.IO;
+﻿using System;
 
 public class Program
 {
     public static void Main()
     {
+        bool isHuman = true;
+
+        bool f = false;
+
+        decimal num = 9.99m;
+
+        decimal total = num * num;
+
+        string firstName = "jackie";
+        string lastName = "earhart";
+        int age = 34;
+        string job = "photographer";
+        string favoriteBand = "AC/DC";
+        string favoriteSportsTeam = "Boston Red Sox";
+
+        Console.WriteLine("First Name:" + firstName);
+        Console.WriteLine("Last Name:" + lastName);
+        Console.WriteLine("Age:" + age);
+        Console.WriteLine("Job:" + job);
+        Console.WriteLine("Favorite Band:" + favoriteBand);
+        Console.WriteLine("Favorite Sports Team:" + favoriteSportsTeam);
+
+        int myInterger = (int)num;
+        Console.WriteLine("My Interger:" + num);
+
+
+
+
         // your code goes here
 
 
